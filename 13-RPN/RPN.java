46e71ea6ce8f5f95312fb38f8335f020c3a71310
wileyrong <rileywong96@gmail.com>
2014-04-08 08:35:48 -0400

    RPN

--- 13-RPN/RPN.java
+++ 13-RPN/RPN.java
@@ -1,4 +1,14 @@
 public class RPN{
-    
+    public static void main(String[]args){
+	MyStack tmp = new MyStack();
+    Scanner sc = new Scanner(System.in);
+     String i = sc.nextString();
+     int n1 = Integer.parseInt(i.nextLine());
+     //if(i.nextLine().equals("+")){
+	 
+
+
+
+    }
 
 }
