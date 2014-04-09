343829696907fa46ff92c78d2462d47278ebf6ab
wileyrong <rileywong96@gmail.com>
2014-04-08 21:29:54 -0400

    Updated it works now yay :)

--- 13-RPN/RPN.java
+++ 13-RPN/RPN.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
 public class RPN{
-    public static void main(String[]args){
-	MyStack tmp = new MyStack();
+  public static void main(String[]args){
+    MyStack tmp = new MyStack();
     Scanner sc = new Scanner(System.in);
-     String i = sc.nextString();
-     int n1 = Integer.parseInt(i.nextLine());
-     //if(i.nextLine().equals("+")){
-	 
-
-
-
+    boolean lifesucks = true;
+    //if(i.nextLine().equals("+")){
+    while(lifesucks){
+      String i = sc.nextLine();
+      if(i.equals("+")){
+        int tmp1 = Integer.parseInt(tmp.pop());
+        int tmp2 = Integer.parseInt(tmp.pop());
+        int tmp3 = tmp1 + tmp2;
+        tmp.push(Integer.toString(tmp3));
+        System.out.println(tmp);
+      }
+      else if(i.equals("-")){
+        int tmp1 = Integer.parseInt(tmp.pop());
+        int tmp2 = Integer.parseInt(tmp.pop());
+        int tmp3 = tmp1 - tmp2;
+        tmp.push(Integer.toString(tmp3));
+        System.out.println(tmp);
+      }
+      else if(i.equals("*")){
+        int tmp1 = Integer.parseInt(tmp.pop());
+        int tmp2 = Integer.parseInt(tmp.pop());
+        int tmp3 = tmp1 + tmp2;
+        tmp.push(Integer.toString(tmp3));
+        System.out.println(tmp);
+      }
+      else if(i.equals("/")){
+        int tmp1 = Integer.parseInt(tmp.pop());
+        int tmp2 = Integer.parseInt(tmp.pop());
+        int tmp3 = tmp1 + tmp2;
+        tmp.push(Integer.toString(tmp3));
+        System.out.println(tmp);
+      }
+      else{
+        int tmp1 = Integer.parseInt(i);
+        tmp.push(Integer.toString(tmp1));
+        System.out.println(tmp);
+        
+      }
     }
-
+  }
+  
 }
