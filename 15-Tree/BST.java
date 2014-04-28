55154e2982fdfdcf6e57b45874fb187519e8fb05
wileyrong <rileywong96@gmail.com>
2014-04-27 22:05:23 -0400

    Update BST.java

--- 15-Tree/BST.java
+++ 15-Tree/BST.java
@@ -1,31 +1,47 @@
 public class BST{
-    private Node root;
-    
-    public int search(int x){
-	Node p = root;
-	while(p != null && p.getData() != x){
-	    if(p.getData() > x){
-		p = p.getLeft();
-	    }
-	    else {
-		p = p.getRight();
-	    }
-	}
-	return p;	    
+  private Node root;
+  
+  public BST(){
+    root = null;
+  }
+  
+  public int search(int x){
+    Node p = root;
+    while(p != null && p.getData() != x){
+      if(p.getData() > x){
+        p = p.getLeft();
+      }
+      else {
+        p = p.getRight();
+      }
     }
-
-    public void insert(Node n){
-	Node temp = root;
-	boolean p;
-	while(temp != null){
-	    if(temp.getData() > n.getData()){
-		temp = temp.getLeft();
-		p = true;
-	    }
-	    else {
-		temp = temp.getRight();
-		p = false;
-	    }
-	}
-
+    return p.getData();     
+  }
+  
+  public void insert(Node n){
+    Node p = root;
+    while(p != null && p.getData() != n.getData()){
+      if(p.getData() > n.getData()){
+        p = p.getLeft();
+      }
+      else {
+        p = p.getRight();
+      }
+    }
+    p = n;
+  }
+  
+  public Node search2(Node c, int x){
+    Node p = root;
+    while(p != null && p.getData() != x){
+      if(p.getData() > x){
+        search2(p.getLeft(), x);
+      }
+      else {
+        search2(p.getRight(), x);
+      }
+    }
+    return p;
+  }
+  
 }
