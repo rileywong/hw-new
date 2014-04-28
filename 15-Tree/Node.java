1344a3e13c7f5bc34b1a7a58249a64c991a8cc48
wileyrong <rileywong96@gmail.com>
2014-04-27 22:06:31 -0400

    Update Node.java

--- 15-Tree/Node.java
+++ 15-Tree/Node.java
@@ -1,40 +1,40 @@
 public class Node{
-    private String data;
-    private Node left, right;
-    
-    public Node(String s){
-	data = s;
-	left = null
-	right = null;
-    }
-    
-    public void setData()(String s){
-	data = s;
-    }
-   
-    public String getData(){
-      	return data;
-    }
-    
-    public void setLeft(Node n){
-	left = n;
-    }
-
-    public void setRight(Node n){
-	right = n;
-    }
-
-    public Node getRight(){
-	return right;
-    }
-    
-    public Node getLeft(){
-	return left;
-    }
-
-    public String toString(){
-	return "" + data;
-    }
-
+  private int data;
+  private Node left, right;
+  
+  public Node(int s){
+    data = s;
+    left = null;
+    right = null;
+  }
+  
+  public void setData(int s){
+    data = s;
+  }
+  
+  public int getData(){
+    return data;
+  }
+  
+  public void setLeft(Node n){
+    left = n;
+  }
+  
+  public void setRight(Node n){
+    right = n;
+  }
+  
+  public Node getRight(){
+    return right;
+  }
+  
+  public Node getLeft(){
+    return left;
+  }
+  
+  public String toString(){
+    return "" + data;
+  }
+  
   
 }
