22272770ccdeafd055ad56a06676aad783488459
wileyrong <rileywong96@gmail.com>
2014-05-12 08:27:56 -0400

    heaps

--- 18-Heaps/Heaps.java
+++ 18-Heaps/Heaps.java
@@ -1,4 +1,11 @@
 public class Heaps{
-    
+    public void add(int n){
+    }
+
+    public void remove(int n){
+    }
+
+    public String toString(){
+    }
 
 }
--- 18-Heaps/Node.java
+++ 18-Heaps/Node.java
@@ -1,42 +1,40 @@
 public class Node{
-    private String data;
-    private Node next;
-    
-    public Node(String s){
-	data = s;
-    }
-    
-    public void setName(String s){
-	data = s;
-    }
-   
-    public String getData(){
-	return data;
-    }
-    
-    public void setNext(Node n){
-	next = n;
-    }
-
-    public Node getNext(){
-	return next;
-    }
-
-    public String toString(){
-	return "Data: " + getData() + " Next: " + getNext();
-    }
-
-    public static void main(String[] args){
-	Node n1;
-	n1 = new Node("Tommy");
-	System.out.println(n1);
-	Node n2 = new Node("Sammy");
-	Node n3 = new Node("Clyde");
-	n2.setNext(n3);
-	System.out.println(n2);
-	n1.setNext(n2);
-	System.out.println(n1.getNext());
-	System.out.println(n2.getNext());
-    }
-
+  private int data;
+  private Node left, right;
+  
+  public Node(int s){
+    data = s;
+    left = null;
+    right = null;
+  }
+  
+  public void setData(int s){
+    data = s;
+  }
+  
+  public int getData(){
+    return data;
+  }
+  
+  public void setLeft(Node n){
+    left = n;
+  }
+  
+  public void setRight(Node n){
+    right = n;
+  }
+  
+  public Node getRight(){
+    return right;
+  }
+  
+  public Node getLeft(){
+    return left;
+  }
+  
+  public String toString(){
+    return "" + data;
+  }
+  
+  
 }
