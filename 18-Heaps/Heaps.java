d24229631f426a671210cbd0bc44ad5c70465aa8
wileyrong <rileywong96@gmail.com>
2014-05-15 08:27:50 -0400

    Maps

--- 18-Heaps/Heaps.java
+++ 18-Heaps/Heaps.java
@@ -1,5 +1,19 @@
 public class Heaps{
+    Node head;
+    
+    public Head(){
+	head = null;
+    }
+   
     public void add(int n){
+	Node temp = new Node(n);
+	if(head.getData() == null){
+	    head = temp;
+	    return;
+	}
+	while(temp.getData() != null){
+	    
+	    
     }
 
     public void remove(int n){
